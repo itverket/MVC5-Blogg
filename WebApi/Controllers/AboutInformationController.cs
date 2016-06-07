75f61b898c26d59093c92612cdca0e64bdb636c5
Ole Andreas Rydland <Ole Andreas Rydland>
2016-06-07 23:26:02 +0200

    added WEB API tasks and solution (copy tasks to unfinished solution later)

---
+++ WebApi/Controllers/AboutInformationController.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationModel.Repositories;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class AboutInformationController : ApiController
+    {
+        private AboutRepository _aboutRepository = new AboutRepository();
+
+        public AboutInformationModel Get()
+        {
+            var repository = new AboutRepository();
+            var information = repository.GetAboutInformation();
+
+            return new AboutInformationModel
+            {
+                AboutMe = information.AboutMe,
+                BirthDate = information.BirthDate,
+                FirstName = information.FirstName,
+                LastName = information.LastName
+            };
+        }
+    }
+}
---
+++ WebApi/Models/AboutInformationModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApi.Models
+{
+    public class AboutInformationModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string AboutMe { get; set; }
+    }
+}
